b4346ef1eacaac5e2c59671e71ac77f8e3ca6ccc
Jacek <jac.malek@gmail.com>
2021-06-09 12:02:18 +0200

    Sending email done

--- src/main/java/pl/coderslab/charity/controller/UserController.java
+++ src/main/java/pl/coderslab/charity/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.dto.EmailDto;
@@ -50,4 +51,5 @@ public class UserController {
         userService.saveUser(userDto);
         return "redirect:/";
     }
+
 }
--- src/main/webapp/WEB-INF/views/register.jsp
+++ src/main/webapp/WEB-INF/views/register.jsp
@@ -26,7 +26,7 @@
         </div>
 
         <div class="form-group form-group--buttons">
-            <a href="/login" class="btn btn--without-border">Zaloguj się</a>
+            <a href="/user/login" class="btn btn--without-border">Zaloguj się</a>
             <button class="btn" type="submit">Załóż konto</button>
         </div>
 
